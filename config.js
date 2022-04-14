b7d2991893ce358cd22ba6620545b0c2ba1a7a98
Satria356 <memeindo74@gmail.com>
2022-04-14 15:26:46 +0700

    Satria356

---
+++ config.js
@@ -0,0 +1,52 @@
+/**
+   * Error Chat Gua !!!
+   wa.me/67572919291
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapi.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapi.xyz': 'Your Key',
+}
+
+// Other
+global.owner = ['6283811034750','6283811034750']
+global.web = 'https://youtube.com/Domge'
+global.bot = 'MLBB-BOT'
+global.premium = ['6283811034750']
+global.packname = 'Hisoka Morrow'
+global.author = 'WhatsApp Bot'
+global.sessionName = 'hisoka'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '≥'
+global.mess = {
+    success: 'Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: '_Please Aa Waiting...._',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 15
+}
+global.thumb = fs.readFileSync('./media/kahfz.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
